5b1c9622e98eb2e018f265a958ba82d6d5b6576e
nizar <nizardjassim@gmail.com>
2024-02-23 23:55:32 +0100

    prisma

--- app/(pages)/blog/page.jsx
+++ app/(pages)/blog/page.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import styles from './styles.module.css'
-const Page = () => {
+import prisma from '@/lib/prisma'
+
+async function getPosts(){
+  const posts = await prisma.post.findMany({
+    where: {published: true},
+    include: {
+      author:{
+        select: {name: true}
+      }
+    }
+  })
+
+  return posts
+}
+
+const Page = async() => {
+
+  const posts = await getPosts()
+  console.log(posts);
   return (
     <main className={styles.main}>
 
         <div className={styles.innerCo}>
         <h1>BLOG</h1>
+
+        <div className={styles.content}>
+          {posts.map((post) => (
+            <div key={post.id} className={styles.post}>
+              <h2>{post.title}</h2>
+              <p>By {post.author.name}</p>
+              <p>{post.content}</p>
+            </div>
+          ))}
+        </div>
         </div>
        
     </main>
